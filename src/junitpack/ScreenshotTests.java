bb1dc7d0d77ce21cec4eff9f25e9eab9486d1cb5
alex1212yang <alex_yang@tutk.com>
2017-03-15 16:07:41 +0800

    315
    315

--- src/junitpack/ScreenshotTests.java
+++ src/junitpack/ScreenshotTests.java
@@ -27,14 +27,15 @@ public class ScreenshotTests {
 		@Test
 		public void Account_Setting() throws Exception {
 					//System.setProperty("webdriver.gecko.driver", "C:\\Program Files (x86)\\Jenkins\\workspace\\junit_web\\geckodriver.exe");
-					service = new ChromeDriverService.Builder().usingDriverExecutable(new File("D:\\ChromeDriver.exe"))
-			        .usingAnyFreePort().build();
-					
+				//service = new ChromeDriverService.Builder().usingDriverExecutable(new File("D:\\ChromeDriver.exe"))
+			      //  .usingAnyFreePort().build();
+					service = new ChromeDriverService.Builder().usingDriverExecutable(new File("D:\\geckodriver.exe"))
+					        .usingAnyFreePort().build();
 					//System.setProperty("webdriver.ie.driver", "C:/Users/alexyang/workspace/junitproject_kalay_vsaas_3.0/IEDriverServer.exe");
 					//System.setProperty("webdriver.chrome.driver", "C:/Users/alexyang/workspace/junitproject_kalay_vsaas_3.0/ChromeDriver.exe");
-					//WebDriver driverChrome= new FirefoxDriver();
+					WebDriver driverChrome= new FirefoxDriver();
 					//WebDriver driverChrome= new InternetExplorerDriver();
-					WebDriver driverChrome= new ChromeDriver();
+					//WebDriver driverChrome= new ChromeDriver();
 					
 					
 					WebDriverWait wait = new WebDriverWait(driverChrome, 10);
@@ -45,5 +46,8 @@ public class ScreenshotTests {
 					File scrFile = ((TakesScreenshot)driverChrome).getScreenshotAs(OutputType.FILE);
 					// Now you can do whatever you need to do with it, for example copy somewhere
 					FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
+					
+					driverChrome.close();
+					driverChrome.quit();
 }
 	}
