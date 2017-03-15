1b3815c3ef4ed9c43f7fae9c1a96a0b8db40c0c4
alex1212yang <alex_yang@tutk.com>
2017-03-15 18:24:19 +0800

    thgth
    fghfgh

--- src/junitpack/ScreenshotTests.java
+++ src/junitpack/ScreenshotTests.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerDriverService;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.Proxy.ProxyType;
@@ -21,16 +23,15 @@ import org.openqa.selenium.support.PageFactory;
 import java.io.File;
 
 public class ScreenshotTests {
-	private static ChromeDriverService service;
+	private static GeckoDriverService service;
+	
     private WebDriver driver;
 
 		@Test
 		public void Account_Setting() throws Exception {
-					//System.setProperty("webdriver.gecko.driver", "C:\\Program Files (x86)\\Jenkins\\workspace\\junit_web\\geckodriver.exe");
-				//service = new ChromeDriverService.Builder().usingDriverExecutable(new File("D:\\ChromeDriver.exe"))
-			      //  .usingAnyFreePort().build();
-					service = new ChromeDriverService.Builder().usingDriverExecutable(new File("D:\\IEDriverServer.exe"))
-					        .usingAnyFreePort().build();
+					System.setProperty("webdriver.ie.driver", "D:\\IEDriverServer.exe");
+					//service = new ChromeDriverService.Builder().usingDriverExecutable(new File("D:\\ChromeDriver.exe")).usingAnyFreePort().build();
+					//service = new GeckoDriverService.Builder().usingDriverExecutable(new File("D:\\geckodriver.exe")).usingAnyFreePort().build();
 					//System.setProperty("webdriver.ie.driver", "C:/Users/alexyang/workspace/junitproject_kalay_vsaas_3.0/IEDriverServer.exe");
 					//System.setProperty("webdriver.chrome.driver", "C:/Users/alexyang/workspace/junitproject_kalay_vsaas_3.0/ChromeDriver.exe");
 					//WebDriver driverChrome= new FirefoxDriver();
@@ -42,9 +43,10 @@ public class ScreenshotTests {
 					driverChrome.manage().window().maximize();
 					driverChrome.navigate().to("https://alpha.kalay.us/");
 					
-				
+					Thread.sleep(10000);
 					File scrFile = ((TakesScreenshot)driverChrome).getScreenshotAs(OutputType.FILE);
 					// Now you can do whatever you need to do with it, for example copy somewhere
+					
 					FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
 					
 					driverChrome.close();
